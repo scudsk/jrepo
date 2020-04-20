f69ac7fca1a7aa7a8f8050a06c472c96548b3b2c
scudsk <scudsk@gmail.com>
2020-04-20 20:19:45 +0530

    Test Commit

---
+++ test/test.py
@@ -0,0 +1,15 @@
+import unittest, sys, os
+sys.path.append(os.path.abspath("./src"))
+from arrayPartition1 import Solution
+
+class TestArrayPartition1(unittest.TestCase):
+    def setUp(self):
+        self.sln = Solution()
+
+    def test_basic(self):
+        self.assertEqual(self.sln.ArrayPairSum([1,4,2,3]), 0)
+
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
